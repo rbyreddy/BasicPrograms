5d767c30f9c34446442c789204319b9192c3df68
Rakesh <byreddyrakesh41@gmail.com>
2022-10-19 11:55:11 +0530

    ADDED: Code for the Leap Year Program

---
+++ src/basic/programs/LeapYear.java
@@ -0,0 +1,21 @@
+package basic.programs;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type any year: ");
+        int year = sc.nextInt();
+
+
+        if (year % 4 == 0 || year % 100 == 0 || year % 400 == 0) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not Leap year.");
+        }
+
+    }
+}
